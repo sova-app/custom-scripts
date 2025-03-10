a61397fb71a4e5c67cf49ffd15c466a3268dad51
bl1nkker <nurovich14@gmail.com>
2025-03-10 21:09:26 +0500

    fix: added custom claims update

--- custom.js
+++ custom.js
@@ -1,11 +1,16 @@
 import admin from "firebase-admin";
 import serviceAccount from "./fuck.json" with { type: "json" };
 
+const USERID = "sjGhOhzg01WODQLGPKcndX4hmZG2"
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
-let user = await admin.auth().getUser("<user-id>")
-console.log(user)
+let user = await admin.auth().getUser(USERID)
+console.log("claims before:", user["customClaims"])
 const claims = {};
 claims['role'] = 'driver';
+
+await admin.auth().setCustomUserClaims(USERID, claims);
+user = await admin.auth().getUser(USERID)
+console.log("claims after:", user["customClaims"])
